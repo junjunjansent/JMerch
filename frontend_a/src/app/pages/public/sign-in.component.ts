13da284f018ebda5b8b1589d602946fb63f01b33
Jansen <57595871+junjunjansent@users.noreply.github.com>
2025-07-17 12:40:38 +0800

    Amended frontend

--- frontend_a/src/app/pages/public/sign-in.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sign-in',
-  standalone: true,
-  templateUrl: './sign-in.component.html',
-  //   styleUrls: ['./home.component.css'],
-})
-export class SignInComponent {}
