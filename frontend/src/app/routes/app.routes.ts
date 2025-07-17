086d6cf1ca2522ebf65d596433d83c5ea647fdbb
Jansen <57595871+junjunjansent@users.noreply.github.com>
2025-07-17 12:39:25 +0800

    Changed Frontend

--- frontend/src/app/routes/app.routes.ts
+++ frontend/src/app/routes/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/public/home.component';
-import { SignUpComponent } from './pages/public/sign-up.component';
-import { SignInComponent } from './pages/public/sign-in.component';
+import { HomeComponent } from '../pages/public/home.component';
+import { SignUpComponent } from '../pages/public/sign-up.component';
+import { SignInComponent } from '../pages/public/sign-in.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
