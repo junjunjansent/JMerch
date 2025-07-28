0a244662a9529ecc96b9d29d072aa21e90b2a4b8
Jansen <57595871+junjunjansent@users.noreply.github.com>
2025-07-28 17:28:06 +0800

    Amending angular deployment

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import HomeComponent from './pages/index.page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  imports: [RouterOutlet, HomeComponent],
+  template: `<app-home /><router-outlet />`,
 })
 export class AppComponent {}
